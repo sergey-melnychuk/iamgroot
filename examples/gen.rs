a02c80ab43e11a8b2a292fadca7e594e98980d6e
Sergey Melnychuk <serg.meln@gmail.com>
2023-02-24 14:05:35 +0100

    example: simplify the example

--- examples/gen.rs
+++ examples/gen.rs
@@ -9,56 +9,16 @@ fn main() {
 
     let json = serde_json::json!({
         "jsonrpc": "2.0",
-        "method": "starknet_call",
-        "params": {
-            "request": {
-                "contract_address": "1",
-                "calldata": ["2"],
-                "entry_point_selector": "3"
-            },
-            "block_id": "0xFF"
-        },
-        "id": 42
+        "method": "starknet_blockNumber",
+        "params": [],
     });
-    println!(">>> {}", json.to_string());
 
     let req: jsonrpc::Request = serde_json::from_value(json).unwrap();
-    let ret = gen::handle(&state, req.with_id(jsonrpc::Id::Number(42)));
-    println!("<<< {}", serde_json::to_string(&ret).unwrap());
-
-    let req = jsonrpc::Request::new(
-        "starknet_call".to_string(),
-        serde_json::json!([
-            {
-                "contract_address": "1",
-                "calldata": ["2"],
-                "entry_point_selector": "3"
-            },
-            "0xFF"
-        ]),
-    );
-    println!("\n>>> {}", serde_json::to_string(&req).unwrap());
+    let req = req.with_id(jsonrpc::Id::Number(1));
+    println!(">>> {}", serde_json::to_string(&req).unwrap());
     let ret = gen::handle(&state, req);
     println!("<<< {}", serde_json::to_string(&ret).unwrap());
 }
-
-// 0. (git restore examples/gen.rs)
-// 1. cargo run --release -- ./api/input.openrpc CODE >> examples/gen.rs
-// 2. implement gen::Rpc for State and 'starknet_call' method (see below)
-//    - consider running 'cargo fmt' along the way
-// 3. cargo run --example gen
-
-// starknet_call:
-/*
-        let mut x = self.0.borrow_mut();
-        *x += 1;
-
-        if *x % 2 == 0 {
-            Ok(vec![format!("x={x}")])
-        } else {
-            Err(jsonrpc::Error::new(-42, "Not implemented".to_string()))
-        }
-*/
 
 #[allow(unused_variables)]
 impl gen::Rpc for State {}
