3528507b0397666264ed173b5cc40841c217212c
Sergey Melnychuk <serg.meln@gmail.com>
2023-02-24 23:56:05 +0100

    example: add method calls

--- examples/gen.rs
+++ examples/gen.rs
@@ -5,16 +5,58 @@ struct State;
 fn main() {
     let state = State;
 
-    let json = serde_json::json!({
-        "jsonrpc": "2.0",
-        "method": "starknet_blockNumber",
-        "params": [],
-    });
+    call(
+        &state,
+        1,
+        serde_json::json!({
+            "jsonrpc": "2.0",
+            "method": "starknet_blockNumber",
+            "params": [],
+        }),
+    );
 
+    call(
+        &state,
+        2,
+        serde_json::json!({
+            "jsonrpc": "2.0",
+            "method": "starknet_call",
+            "params": [
+                {
+                    "entry_point_selector": "uno",
+                    "calldata": ["due"],
+                    "contract_address": "tre"
+                },
+                42
+            ]
+        }),
+    );
+
+    call(
+        &state,
+        3,
+        serde_json::json!({
+            "jsonrpc": "2.0",
+            "method": "starknet_call",
+            "params": {
+                "request": {
+                    "entry_point_selector": "one",
+                    "calldata": ["two"],
+                    "contract_address": "three"
+                },
+                "block_id": "hi"
+            }
+        }),
+    );
+}
+
+fn call<T: gen::Rpc>(rpc: &T, id: i64, json: serde_json::Value) {
     let req: jsonrpc::Request = serde_json::from_value(json).unwrap();
-    let req = req.with_id(jsonrpc::Id::Number(1));
-    println!(">>> {}", serde_json::to_string(&req).unwrap());
-    let ret = gen::handle(&state, req);
+    let req = req.with_id(jsonrpc::Id::Number(id));
+    let json = serde_json::to_string(&req).unwrap();
+    println!("\n>>> {}", json);
+    let req: jsonrpc::Request = serde_json::from_str(&json).unwrap();
+    let ret = gen::handle(rpc, req);
     println!("<<< {}", serde_json::to_string(&ret).unwrap());
 }
 
