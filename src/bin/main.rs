8c8ec07e73ac26fe07ea9e813e7fd8c8325f06f1
Sergey Melnychuk <serg.meln@gmail.com>
2023-02-27 18:37:48 +0100

    code: extract gen json/tree/code functions

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,44 +1,4 @@
-use std::collections::HashMap;
-
-use openrpc_stub_gen::{binding, openrpc, renders};
-
-fn run(
-    spec: openrpc::OpenRpc,
-    cache: &mut HashMap<String, binding::Binding>,
-) -> Vec<binding::Contract> {
-    let bindings = spec
-        .components
-        .as_ref()
-        .expect("components")
-        .schemas
-        .iter()
-        .map(|(name, schema)| binding::get_schema_binding(name.to_string(), schema, &spec, cache))
-        .flat_map(binding::unfold_binding)
-        // Make a second pass (extra unfolding might be necessary)
-        .flat_map(binding::unfold_binding)
-        .collect::<Vec<_>>();
-
-    for b in bindings {
-        cache.insert(b.get_name(), b);
-    }
-
-    let contracts = spec
-        .methods
-        .iter()
-        .filter_map(|method| {
-            let name = method.name.clone();
-            binding::get_method_contract(name, &spec, cache)
-        })
-        .collect::<Vec<_>>();
-
-    contracts
-}
-
-fn parse(path: &str) -> openrpc::OpenRpc {
-    log::info!("Processing file: {path}");
-    let json = std::fs::read_to_string(path).expect("JSON file exists and is readable.");
-    serde_json::from_str(&json).expect("json")
-}
+use openrpc_stub_gen::{gen_code, gen_json, gen_tree};
 
 fn main() {
     env_logger::init();
@@ -60,68 +20,11 @@ fn main() {
             eprintln!("JSON mode supports only single file");
             return;
         }
-        let spec = parse(&paths[0]);
-        let text = serde_json::to_string(&spec).expect("json");
-        println!("{text}");
+        println!("{}", gen_json(&paths[0]));
     } else if mode.as_str() == "TREE" {
-        let mut cache = HashMap::new();
-
-        let contracts = paths
-            .into_iter()
-            .map(|path| parse(&path))
-            .flat_map(|spec| run(spec, &mut cache))
-            .collect::<Vec<_>>();
-
-        cache
-            .iter()
-            .for_each(|(name, binding)| println!("---\n{name}: {binding:#?}"));
-
-        contracts
-            .iter()
-            .for_each(|contract| println!("---\n{contract:#?}"));
+        println!("{}", gen_tree(&paths));
     } else if mode.as_str() == "CODE" {
-        let mut cache = HashMap::new();
-
-        let contracts = paths
-            .into_iter()
-            .map(|path| parse(&path))
-            .flat_map(|spec| run(spec, &mut cache))
-            .collect::<Vec<_>>();
-
-        println!("// vvv GENERATED CODE BELOW vvv");
-        println!("#[allow(dead_code)]");
-        println!("#[allow(non_snake_case)]");
-        println!("#[allow(unused_variables)]");
-        println!("mod gen {{");
-        println!("use serde::{{Deserialize, Serialize}};");
-        println!("use serde_json::Value;");
-        println!("\nuse openrpc_stub_gen::jsonrpc;");
-
-        for (name, binding) in &cache {
-            let code = renders::render_object(name, binding)
-                .unwrap_or_else(|e| format!("// ERROR: Rendering object '{name}' failed: {e}"));
-
-            if !code.is_empty() {
-                println!("\n// object: '{name}'\n{code}");
-            }
-        }
-
-        println!("\npub trait Rpc {{");
-        for contract in &contracts {
-            let code = renders::render_method(&contract.name, contract);
-            println!("\n{code}");
-        }
-        println!("}}");
-
-        for contract in &contracts {
-            let code = renders::render_method_handler(&contract.name, contract);
-            println!("{code}");
-        }
-
-        let handler = renders::render_handle_function(&contracts);
-        println!("{handler}");
-        println!("}}");
-        println!("// ^^^ GENERATED CODE ABOVE ^^^");
+        println!("{}", gen_code(&paths));
     } else {
         eprintln!("Unknown mode: {mode}. Supported are: JSON, TREE, CODE.");
     }
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,123 @@
-pub mod binding;
-pub mod codegen;
+use std::collections::HashMap;
+
+pub(crate) mod binding;
+pub(crate) mod codegen;
 pub mod jsonrpc;
-pub mod openrpc;
-pub mod renders;
+pub(crate) mod openrpc;
+pub(crate) mod renders;
+
+fn extract_contracts(
+    spec: openrpc::OpenRpc,
+    cache: &mut HashMap<String, binding::Binding>,
+) -> Vec<binding::Contract> {
+    let bindings = spec
+        .components
+        .as_ref()
+        .expect("components")
+        .schemas
+        .iter()
+        .map(|(name, schema)| binding::get_schema_binding(name.to_string(), schema, &spec, cache))
+        .flat_map(binding::unfold_binding)
+        // Make a second pass (extra unfolding might be necessary)
+        .flat_map(binding::unfold_binding)
+        .collect::<Vec<_>>();
+
+    for b in bindings {
+        cache.insert(b.get_name(), b);
+    }
+
+    let contracts = spec
+        .methods
+        .iter()
+        .filter_map(|method| {
+            let name = method.name.clone();
+            binding::get_method_contract(name, &spec, cache)
+        })
+        .collect::<Vec<_>>();
+
+    contracts
+}
+
+fn parse(path: &str) -> openrpc::OpenRpc {
+    log::info!("Processing file: {path}");
+    let json = std::fs::read_to_string(path).expect("JSON file exists and is readable.");
+    serde_json::from_str(&json).expect("json")
+}
+
+pub fn gen_json(path: &str) -> String {
+    let spec = parse(path);
+    serde_json::to_string(&spec).expect("json")
+}
+
+pub fn gen_tree(paths: &[String]) -> String {
+    let mut cache = HashMap::new();
+
+    let contracts = paths
+        .iter()
+        .map(|path| parse(path))
+        .flat_map(|spec| extract_contracts(spec, &mut cache))
+        .collect::<Vec<_>>();
+
+    let mut target = String::new();
+    use std::fmt::Write;
+
+    cache
+        .iter()
+        .for_each(|(name, binding)| writeln!(target, "---\n{name}: {binding:#?}").unwrap());
+
+    contracts
+        .iter()
+        .for_each(|contract| writeln!(target, "---\n{contract:#?}").unwrap());
+
+    target
+}
+
+pub fn gen_code(paths: &[String]) -> String {
+    let mut cache = HashMap::new();
+
+    let contracts = paths
+        .iter()
+        .map(|path| parse(path))
+        .flat_map(|spec| extract_contracts(spec, &mut cache))
+        .collect::<Vec<_>>();
+
+    let mut target = String::new();
+    use std::fmt::Write;
+
+    writeln!(target, "// vvv GENERATED CODE BELOW vvv").unwrap();
+    writeln!(target, "#[allow(dead_code)]").unwrap();
+    writeln!(target, "#[allow(non_snake_case)]").unwrap();
+    writeln!(target, "#[allow(unused_variables)]").unwrap();
+    writeln!(target, "mod gen {{").unwrap();
+    writeln!(target, "use serde::{{Deserialize, Serialize}};").unwrap();
+    writeln!(target, "use serde_json::Value;").unwrap();
+    writeln!(target, "\nuse openrpc_stub_gen::jsonrpc;").unwrap();
+
+    for (name, binding) in &cache {
+        let code = renders::render_object(name, binding)
+            .unwrap_or_else(|e| format!("// ERROR: Rendering object '{name}' failed: {e}"));
+
+        if !code.is_empty() {
+            writeln!(target, "\n// object: '{name}'\n{code}").unwrap();
+        }
+    }
+
+    writeln!(target, "\npub trait Rpc {{").unwrap();
+    for contract in &contracts {
+        let code = renders::render_method(&contract.name, contract);
+        writeln!(target, "\n{code}").unwrap();
+    }
+    writeln!(target, "}}").unwrap();
+
+    for contract in &contracts {
+        let code = renders::render_method_handler(&contract.name, contract);
+        writeln!(target, "{code}").unwrap();
+    }
+
+    let handler = renders::render_handle_function(&contracts);
+    writeln!(target, "{handler}").unwrap();
+    writeln!(target, "}}").unwrap();
+    writeln!(target, "// ^^^ GENERATED CODE ABOVE ^^^").unwrap();
+
+    target
+}
