51cc0b7d0d15ca944374413c4f7fdbfe081a7107
Sergey Melnychuk <serg.meln@gmail.com>
2023-03-21 11:39:26 +0100

    eth: add example block

--- examples/eth.rs
+++ examples/eth.rs
@@ -1,6 +1,6 @@
 fn main() {
     {
-        let json = include_str!("../0x10165f5.json");
+        let json = include_str!("../api/0x10165f5.json");
         let mut res: openrpc_stub_gen::jsonrpc::Response = serde_json::from_str(json).unwrap();
         let res: gen::Block = serde_json::from_value(res.result.take().unwrap()).unwrap();
         println!("{res:?}");
