789850bad80604e074f3ac531faa12961a033075
Sergey Melnychuk <serg.meln@gmail.com>
2023-03-30 15:12:49 +0200

    eth: prett-print parsed block

--- examples/eth.rs
+++ examples/eth.rs
@@ -3,7 +3,7 @@ fn main() {
         let json = include_str!("../api/0x10165f5.json");
         let mut res: openrpc_stub_gen::jsonrpc::Response = serde_json::from_str(json).unwrap();
         let res: gen::Block = serde_json::from_value(res.result.take().unwrap()).unwrap();
-        println!("{res:?}");
+        println!("{res:#?}");
     }
 }
 
