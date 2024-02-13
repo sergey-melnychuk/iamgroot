f5399afe190ec726621ea245a517350c1c1a0f67
Sergey Melnychuk <8093171+sergey-melnychuk@users.noreply.github.com>
2024-02-13 22:24:34 +0100

    todo(eth): `Content` -> `ContentOrRef`

--- x.sh
+++ x.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
-rm examples/x.rs
-
-cp examples/gen.rs examples/x.rs
+rm examples/gen.rs
+cp examples/gen.txt examples/gen.rs
 cargo run -- CODE \
     ./api/starknet_query_api_openrpc.json \
     ./api/starknet_write_api_openrpc.json \
-    ./api/starknet_trace_api_openrpc.json >> examples/x.rs
+    ./api/starknet_trace_api_openrpc.json >> examples/gen.rs
 
-# cp examples/eth.rs examples/x.rs
-# cargo run -- CODE ./api/eth/ethereum.json >> examples/x.rs
+# rm examples/eth.rs
+# cp examples/eth.txt examples/eth.rs
+# cargo run -- CODE ./api/eth/ethereum.json >> examples/eth.rs
 
 cargo fmt
-cargo run --example x
+cargo run --example gen
