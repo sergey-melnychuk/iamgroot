28b393596f71c2f73d3f57ffd601dc8da5f30a4e
Sergey Melnychuk <8093171+sergey-melnychuk@users.noreply.github.com>
2024-02-13 02:46:46 +0100

    feat(eth): fix the spec

--- x.sh
+++ x.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 rm examples/x.rs
-echo 'fn main() { println!("OK"); }' > examples/x.rs
-# cargo run -- CODE ./api/eth/ethereum.json >> examples/x.rs
+
+cp examples/gen.rs examples/x.rs
 cargo run -- CODE \
     ./api/starknet_query_api_openrpc.json \
     ./api/starknet_write_api_openrpc.json \
     ./api/starknet_trace_api_openrpc.json >> examples/x.rs
+
+# cp examples/eth.rs examples/x.rs
+# cargo run -- CODE ./api/eth/ethereum.json >> examples/x.rs
+
 cargo fmt
 cargo run --example x
