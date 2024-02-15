378ac2aa51dcf0285ca7e016bfa551981f762eef
Sergey Melnychuk <8093171+sergey-melnychuk@users.noreply.github.com>
2024-02-15 15:01:46 +0100

    feat(async): add async `Rpc` trait & client

--- src/bin/iamgroot.rs
+++ src/bin/iamgroot.rs
@@ -3,27 +3,41 @@ use iamgroot::{gen_code, gen_json};
 fn main() {
     env_logger::init();
 
-    let (mode, paths) = {
+    let (mode, paths, flags) = {
         let mut args = std::env::args().skip(1);
         let mode = args.next().expect("Output: [JSON, CODE].");
-        let paths = args.collect::<Vec<_>>();
-        (mode, paths)
+        let mut paths = Vec::new();
+        let mut flags = Vec::new();
+        for arg in args {
+            if arg.starts_with("--") {
+                flags.push(arg.strip_prefix("--").unwrap().to_owned());
+            } else {
+                paths.push(arg);
+            }
+        }
+        (mode, paths, flags)
     };
 
-    if paths.is_empty() {
-        eprintln!("No input files");
-        return;
-    }
+    let is_async = flags.iter().any(|flag| flag == "async");
+    let gen_client = flags.iter().any(|flag| flag == "client");
 
-    if mode.as_str() == "JSON" {
-        if paths.len() > 1 {
-            eprintln!("JSON mode supports only single file");
-            return;
+    match mode.as_str() {
+        _ if paths.is_empty() => {
+            eprintln!("No input files");
         }
-        println!("{}", gen_json(&paths[0]));
-    } else if mode.as_str() == "CODE" {
-        println!("{}", gen_code(&paths).unwrap());
-    } else {
-        eprintln!("Unknown mode: {mode}. Supported are: JSON, CODE.");
+        "JSON" if paths.len() == 1 => {
+            let json = gen_json(&paths[0]);
+            println!("{json}");
+        }
+        "JSON" => {
+            eprintln!("JSON mode supports only single file");
+        }
+        "CODE" => {
+            let code = gen_code(&paths, is_async, gen_client).unwrap();
+            println!("{code}");
+        }
+        mode => {
+            eprintln!("Unknown mode: {mode}. Supported are: JSON, CODE.");
+        }
     }
 }
