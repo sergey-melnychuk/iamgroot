20c01b4ef699f34c2165706b0416057718b2d7e8
Sergey Melnychuk <8093171+sergey-melnychuk@users.noreply.github.com>
2024-02-17 21:56:45 +0100

    docs: add server impl example

--- x.sh
+++ x.sh
@@ -15,11 +15,20 @@ cargo run -- CODE \
     ./api/starknet_trace_api_openrpc.json \
     --client >> examples/demo.rs
 
+rm examples/server.rs
+cp examples/server.txt examples/server.rs
+cargo run -- CODE \
+    ./api/starknet_query_api_openrpc.json \
+    ./api/starknet_write_api_openrpc.json \
+    ./api/starknet_trace_api_openrpc.json \
+    --async --client >> examples/server.rs
+
 # rm examples/eth.rs
 # cp examples/eth.txt examples/eth.rs
 # cargo run -- CODE ./api/eth/ethereum.json >> examples/eth.rs
 
 cargo fmt
+cargo build --tests --examples
 
 ## Run 'demo' example (blocking)
 RUST_LOG=debug cargo run --example demo
